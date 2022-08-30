4a9d4e8407712e054f662f4adc334db1cade646f
nohsa97 <81568105+nohsa97@users.noreply.github.com>
2022-08-31 00:22:00 +0900

    1st

---
+++ string.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>String</h1>
+        <?php
+            echo "Hello word";
+        ?>
+
+    <h1>sss</h1>
+        <?php
+            echo "Hello "."word"; 
+        ?>
+    <!-- php는 문자열 더하기에 +가 아닌 .를 이용 -->
+</body>
+</html>
